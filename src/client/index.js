6c6abb78608d10812fe3b3c54f818ea2d2df7bed
Memnoc <smartdroidesign@gmail.com>
2020-03-02 13:26:32 +0000

    sass implementation

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,9 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 
 console.log(checkForName);
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,17 @@ module.exports = {
     entry: "./src/client/index.js",
     module: {
         rules: [{
-            test: '/\.js$',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }]
+                test: '/\.js$',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+
+        ]
+
     },
     plugins: [
         new HtmlWebpackPlugin({
