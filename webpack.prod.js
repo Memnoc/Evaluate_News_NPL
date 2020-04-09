0aae52f91c15480a944927580f91a6f6f4f0599a
Memnoc <smartdroidesign@gmail.com>
2020-04-09 10:09:02 +0100

    MiniCssExtract OptimizeCSSAssets Terser i in prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-    entry: './src/client/index.js',
     mode: 'production',
-    devtool: 'source-map',
-    stats: 'verbose',
+    entry: './src/client/index.js',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     output: {
         libraryTarget: 'var',
         library: 'Client'
@@ -20,7 +23,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     },
@@ -29,14 +32,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
+        new MiniCssExtractPlugin({ filename: '[name].css' })
     ]
 }
